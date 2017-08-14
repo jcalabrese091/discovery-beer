82d98abf8a2c8763eeb5ba6215a87721e7631f8c
Joseph Calabrese <joseph091@dhcp-v076-049.mobile.uci.edu>
2017-08-14 12:10:00 -0700

    update

--- assets/javascript/discoverybeer.js
+++ assets/javascript/discoverybeer.js
@@ -1,48 +1,6 @@
-// $(document).ready( function(){
 
-
-// function xmlToJson(xml) {
-// 	// Create the return object
-// 	var obj = {};
-// 	if (xml.nodeType == 1) { // element
-// 		// do attributes
-// 		if (xml.attributes.length > 0) {
-// 		obj["@attributes"] = {};
-// 			for (var j = 0; j < xml.attributes.length; j++) {
-// 				var attribute = xml.attributes.item(j);
-// 				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-// 			}
-// 		}
-// 	} else if (xml.nodeType == 3) { // text
-// 		obj = xml.nodeValue;
-// 	}
-
-// 	// do children
-// 	// If just one text node inside
-// 	if (xml.hasChildNodes() && xml.childNodes.length === 1 && xml.childNodes[0].nodeType === 3) {
-// 		obj = xml.childNodes[0].nodeValue;
-// 	}
-// 	else if (xml.hasChildNodes()) {
-// 		for(var i = 0; i < xml.childNodes.length; i++) {
-// 			var item = xml.childNodes.item(i);
-// 			var nodeName = item.nodeName;
-// 			if (typeof(obj[nodeName]) == "undefined") {
-// 				obj[nodeName] = xmlToJson(item);
-// 			} else {
-// 				if (typeof(obj[nodeName].push) == "undefined") {
-// 					var old = obj[nodeName];
-// 					obj[nodeName] = [];
-// 					obj[nodeName].push(old);
-// 				}
-// 				obj[nodeName].push(xmlToJson(item));
-// 			}
-// 		}
-// 	}
-// 	return obj;
-// }
-var params=$.param({search:"Oggis"})
-
- var queryURL = "/brewery?"+params;
+var params=$.param({search:"piece"})
+var queryURL = "/brewery?"+params;
 
       console.log(queryURL);
 
@@ -53,16 +11,6 @@ var params=$.param({search:"Oggis"})
         })
 
         .done(function(response) {
-          console.log(response);
-
+          	console.log(response);
 
 });
-// .fail(function (err) {
-// 	console.log(moreErr);
-// }).always(function (something) {
-// 	console.log(something);
-// })
-
-//  // console.log(results);
-//   //$("#uber").text("uber will go here");
-// });
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 var path= require('path');
 var request= require('request');
+var xml2js= require('xml2js');
+var parser = new xml2js.Parser();
 
 app.use("/assets", express.static(__dirname + "/assets"));
 
@@ -12,18 +14,42 @@ app.get('/', function (req, res) {
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-
 app.get('/brewery', function(req, res, next){
 	var brewSearch= req.query.search;
 	console.log(req);
 	request({
-		uri: 'http://api.brewerydb.com/v2/search?key=a38ad29245b0fbac1da3559661f40f6e&q=' +brewSearch+ '&type=Brewery',
-	}).pipe(res);
+		uri: 'http://beermapping.com/webservice/locquery/a3a84df2963679addec260dee07c9f16/'+brewSearch,
+	},
+  function afterRequestIsDone(error, response, body) {
+  	if (error) {
+      return console.error('upload failed:', error);
+    }
+    console.log('Upload successful!  Server responded with:', body);
+    console.log(response);
+  
+  	parser.parseString(body, function afterParseIsDone(err, result) {
+        console.dir(result);
+        console.log('Done');
+        // // var jsonResult = JSON.stringify(result);
+        // console.log(jsonResult);
+        res.send(result.bmp_locations.location[0]);
+    });
+    
+   });
+  
 });
-app.get('/location', function(req, res, next){
-	var brewId= req.query.id;
-	console.log(req);
-	request({
-		uri: 'http://api.brewerydb.com/v2/search?key=a38ad29245b0fbac1da3559661f40f6e&q=' +brewSearch+ '&type=Brewery',
-	}).pipe(res);
-});
+
+// app.get('/brewery', function(req, res, next){
+// 	var brewSearch= req.query.search;
+// 	console.log(req);
+// 	request({
+// 		uri: 'http://api.brewerydb.com/v2/search?key=a38ad29245b0fbac1da3559661f40f6e&q=' +brewSearch+ '&type=Brewery',
+// 	}).pipe(res);
+// });
+// app.get('/location', function(req, res, next){
+// 	var brewId= req.query.id;
+// 	console.log(req);
+// 	request({
+// 		uri: 'http://api.brewerydb.com/v2/search?key=a38ad29245b0fbac1da3559661f40f6e&q=' +brewSearch+ '&type=Brewery',
+// 	}).pipe(res);
+// });
