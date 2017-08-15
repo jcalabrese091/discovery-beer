700780af756430cf7fd25b18c8799983bfde19ce
Joseph Calabrese <joseph091@dhcp-v099-230.mobile.uci.edu>
2017-08-15 14:35:42 -0700

    update

--- assets/javascript/discoverybeer.js
+++ assets/javascript/discoverybeer.js
@@ -1,92 +1,5 @@
-  $("a").click(function(event){
-      // Redirect to Uber API via deep-linking to the mobile web-app
-      var uberURL = "https://m.uber.com/sign-up?";
-      // Add parameters
-      uberURL += "client_id=" + uberClientId;
-      if (typeof userLatitude != typeof undefined) uberURL += "&" + "pickup_latitude=" + userLatitude;
-      if (typeof userLongitude != typeof undefined) uberURL += "&" + "pickup_longitude=" + userLongitude;
-      uberURL += "&" + "dropoff_latitude=" + partyLatitude;
-      uberURL += "&" + "dropoff_longitude=" + partyLongitude;
-      uberURL += "&" + "dropoff_nickname=" + "Brewery";
-      // Redirect to Uber
-      window.location.href = uberURL;
-      });
- $("#selectbrew").on("click", function (event){
-    event.preventDefault();
-var brew = $("#zipcode").val().trim()
-var params=$.param({search:brew})
-var queryURL = "/brewery?"+params;
-
-      console.log(queryURL);
-
-      $.ajax({
-          url: queryURL,
-          method: "GET",
-          crossDomain: true
-        })
-
-        .done(function(response) {
-          	console.log(response);
-        	// var brewData = response.street[0];
-        	// brewInfo.push(brewData)
-        	// console.log(response.street[0]);
-        	// var brewStore = $("<p>");
-        	// var insert= (response.proxylink[0]);
-   			// $("#map").append(image);
-
-
-});
-
-});
-
-        
-
-	var uberClientId = "zb6GeXdmaA_2W_7pLT7-IrYimFqdTUqF";
-	var uberServerToken = "4PXVVIxPxZWsKpvoDwJybCrn7UFd-NsrfncFxpCz";
-        // create placeholder variables
-        var userLatitude
-      , userLongitude
-      , partyLatitude = 33.652751
-      , partyLongitude = -117.846660;
-    navigator.geolocation.watchPosition(function(position) {
-        // Update latitude and longitude
-        console.log(position);
-        userLatitude = position.coords.latitude;
-        userLongitude = position.coords.longitude;
-        // Query Uber API if needed
-        getEstimatesForUserLocation(userLatitude, userLongitude);
-    });
-    function getEstimatesForUserLocation(latitude,longitude) {
-      $.ajax({
-        url: "https://api.uber.com/v1/estimates/price",
-        headers: {
-            Authorization: "Token " + uberServerToken
-        },
-        data: {
-            start_latitude: latitude,
-            start_longitude: longitude,
-            end_latitude: partyLatitude,
-            end_longitude: partyLongitude
-        },
-        success: function(result) {
-            console.log(result);
-            var data = result["prices"];
-		  if (typeof data != typeof undefined) {
-		  // Sort Uber products by time to the user's location
-		  data.sort(function(t0, t1) {
-		    return t0.duration - t1.duration;
-		  });
-		  // Update the Uber button with the shortest time
-		  var shortest = data[0];
-		  if (typeof shortest != typeof undefined) {
-		    console.log("Updating time estimate...");
-		    $("#time").html("IN " + Math.ceil(shortest.duration / 60.0) + " MIN");
-	  		}
-			}
-        }
-      });
-    };
-          var map, infoWindow;
+//<-----------------------------------Google Maps---------------------------------------------->
+var map, infoWindow;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
@@ -131,19 +44,97 @@ var queryURL = "/brewery?"+params;
             map: map
           });
         }
-      }
-//       $("#selectbrew").on("click", function (event){
-//     event.preventDefault();
-//     var brew = $("#zipcode").val().trim();
-// var params=$.param({search:brew})
-// var queryURL = "/brewery?"+params;
-//       console.log(queryURL);
-//       $.ajax({
-//           url: queryURL,
-//           method: "GET",
-//           crossDomain: true
-//         })
-//         .done(function(response) {
-//             console.log(response);
-// });
-// })
+      };
+
+//<-----------------------------------Uber---------------------------------------------->
+
+
+var uberClientId = "zb6GeXdmaA_2W_7pLT7-IrYimFqdTUqF";
+	var uberServerToken = "4PXVVIxPxZWsKpvoDwJybCrn7UFd-NsrfncFxpCz";
+        // create placeholder variables
+        var userLatitude
+      , userLongitude
+      , partyLatitude = 33.652751
+      , partyLongitude = -117.846660;
+    navigator.geolocation.watchPosition(function(position) {
+        // Update latitude and longitude
+        console.log(position);
+        userLatitude = position.coords.latitude;
+        userLongitude = position.coords.longitude;
+        // Query Uber API if needed
+        getEstimatesForUserLocation(userLatitude, userLongitude);
+    });
+    function getEstimatesForUserLocation(latitude,longitude) {
+      $.ajax({
+        url: "https://api.uber.com/v1/estimates/price",
+        headers: {
+            Authorization: "Token " + uberServerToken
+        },
+        data: {
+            start_latitude: latitude,
+            start_longitude: longitude,
+            end_latitude: partyLatitude,
+            end_longitude: partyLongitude
+        },
+        success: function(result) {
+            console.log(result);
+            var data = result["prices"];
+		  if (typeof data != typeof undefined) {
+		  // Sort Uber products by time to the user's location
+		  data.sort(function(t0, t1) {
+		    return t0.duration - t1.duration;
+		  });
+		  // Update the Uber button with the shortest time
+		  var shortest = data[0];
+		  if (typeof shortest != typeof undefined) {
+		    console.log("Updating time estimate...");
+		    $("#time").html("IN " + Math.ceil(shortest.duration / 60.0) + " MIN");
+	  		}
+			}
+        }
+      });
+    };
+  $("a").click(function(event){
+      // Redirect to Uber API via deep-linking to the mobile web-app
+      var uberURL = "https://m.uber.com/sign-up?";
+      // Add parameters
+      uberURL += "client_id=" + uberClientId;
+      if (typeof userLatitude != typeof undefined) uberURL += "&" + "pickup_latitude=" + userLatitude;
+      if (typeof userLongitude != typeof undefined) uberURL += "&" + "pickup_longitude=" + userLongitude;
+      uberURL += "&" + "dropoff_latitude=" + partyLatitude;
+      uberURL += "&" + "dropoff_longitude=" + partyLongitude;
+      uberURL += "&" + "dropoff_nickname=" + "Brewery";
+      // Redirect to Uber
+      window.location.href = uberURL;
+      });
+
+//<-----------------------------------Search---------------------------------------------->
+ 
+ $("#selectbrew").on("click", function (event){
+    event.preventDefault();
+var brew = $("#zipcode").val().trim()
+var params=$.param({search:brew})
+var queryURL = "/brewery?"+params;
+
+      console.log(queryURL);
+
+      $.ajax({
+          url: queryURL,
+          method: "GET",
+          crossDomain: true
+        })
+
+        .done(function(response) {
+          	console.log(response)
+
+		});
+		var googleUrl= "https://maps.googleapis.com/maps/api/geocode/json?address="+brew+ "&key=AIzaSyA8M_UKrxmceM1lc2jBdbX-7AgDCKUlJtg"
+        	$.ajax({
+          	url: googleUrl,
+          	method: "GET",
+        	})
+
+        	.done(function(response) {
+          		console.log(response);
+          	});
+});
